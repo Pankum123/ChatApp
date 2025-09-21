227fd7f70b4c62f84e8b6a1a6893d41e04d2dfea
Pankaj <pka936636@gmail.com>
2025-09-21 17:59:14 +0530

    Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv"
+const app = express()
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
