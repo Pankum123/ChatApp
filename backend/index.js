f3280c20e930ef58bd395744003875d598667b5e
Pankaj <pka936636@gmail.com>
2025-09-21 21:49:54 +0530

    Backend Done

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,34 @@
 import express from "express";
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-const app = express()
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001
-const URL = process.env.MONGODB_URI
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-try{
-mongoose.connect(URL);
-console.log("connected to mongoDB");
+const PORT = process.env.PORT || 4001;
+const URI = process.env.MONGODB_URI;
+
+try {
+  mongoose.connect(URI);
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.log(error);
 }
-catch(error){
-console.log("Error : ",error);
-}
 
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
+});
