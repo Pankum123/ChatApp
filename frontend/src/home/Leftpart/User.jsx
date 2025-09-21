2d59077a241d7bfe10fe3eceb9f96070e9079ebd
Pankaj <pka936636@gmail.com>
2025-09-21 15:27:13 +0530

    left right update

--- frontend/src/home/Leftpart/User.jsx
+++ frontend/src/home/Leftpart/User.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-
-function User() {
+import useConversation from "../../zustand/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import profile from "../../../public/user.jpg";
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
   return (
-    <div>
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
       <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
-        <div className="avatar">
-          <div className="w-15 rounded-full">
-            <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+            <img src={profile} />
           </div>
         </div>
         <div>
-          <h1 className="font-bold">Pankaj</h1>
-          <span>panko@123.com</span>clear
+          <h1 className=" font-bold">{user.fullname}</h1>
+          <span>{user.email}</span>
         </div>
       </div>
     </div>
