ae011ff7213cc3b2f4631f492f53e53b8643eac3
Pankaj <pka936636@gmail.com>
2025-09-21 18:10:32 +0530

    MongoDb connected

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001
+const URL = process.env.MONGODB_URI
+
+try{
+mongoose.connect(URL);
+console.log("connected to mongoDB");
+}
+catch(error){
+console.log("Error : ",error);
+}
 
 
 app.listen(PORT, () => {
